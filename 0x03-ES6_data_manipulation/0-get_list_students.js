66baf31ed673e22bc5628068c7a157bbe3bf92ec
Ahmed Rady <rady20991@gmail.com>
2024-10-08 20:24:25 +0000

    Basic list of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,21 @@
+export default function getListStudents() {
+  const obJ1 = {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  };
+
+  const obJ2 = {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  };
+
+  const obJ3 = {
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  };
+
+  return [obJ1, obJ2, obJ3];
+}
