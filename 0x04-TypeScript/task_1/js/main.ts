5eab09f7d030c89324b8cea79536ed8b6f86898f
Ahmed Rady <rady20991@gmail.com>
2024-10-09 20:44:44 +0000

    Writing a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -35,6 +35,34 @@ const director: Directors = {
   numberOfReports: 17,
 };
 
+interface IStudentClass {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements IStudentClass {
+  private firstName: string;
+  private lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+const student = new StudentClass('John', 'Doe');
+console.log(student.displayName());
+console.log(student.workOnHomework());
+
+
 teacher.contract = true;
 teacher.yearsOfExperience = 5;
 
