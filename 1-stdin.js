60d290e7ffa79771deacf18b58b152e1081deabe
Ahmed Rady <rady20991@gmail.com>
2024-11-27 01:04:00 +0200

    Using Process stdin

---
+++ 1-stdin.js
@@ -0,0 +1,13 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const user_name = process.stdin.read();
+
+  if (user_name) {
+    process.stdout.write(`Your name is: ${user_name}`);
+  }
+});
+
+process.stdin.on('close', () => {
+  process.stdout.write('This important software is now closing\n');
+});
