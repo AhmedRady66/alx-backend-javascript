e46b4cf8e02c23f49d3abef9b84447bd8456b04c
Ahmed Rady <rady20991@gmail.com>
2024-11-28 18:21:27 +0200

    Async tests with done

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,21 +1,18 @@
+const mocha = require('mocha');
+const { expect } = require('chai');
+const sinon = require('sinon');
+
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', () => {
-  it('should return successful response when success is true', (done) => {
+  it('should resolve promise if success is true', (done) => {
     getPaymentTokenFromAPI(true)
-      .then((response) => {
-        expect(response.data).toBe('Successful response from the API');
+      .then((res) => {
+        expect(res).to.eql({ data: 'Successful response from the API' });
         done();
       })
-      .catch(done);
-  });
-
-  it('should not return anything when success is false', (done) => {
-    getPaymentTokenFromAPI(false)
-      .then((response) => {
-        expect(response).toBeUndefined();
-        done();
-      })
-      .catch(done);
+      .catch((err) => {
+        done(err);
+      });
   });
 });
