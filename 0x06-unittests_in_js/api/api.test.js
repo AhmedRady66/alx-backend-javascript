b22b01a5cf30a3b9ce3c13cae5acbc8d4477b7f9
Ahmed Rady <rady20991@gmail.com>
2024-11-28 18:31:52 +0200

    Basic Integration testing

---
+++ 0x06-unittests_in_js/api/api.test.js
@@ -0,0 +1,19 @@
+const request = require('supertest');
+const app = require('./api');
+
+describe('Index Page', () => {
+  it('should return correct status code', async () => {
+    const res = await request(app).get('/');
+    expect(res.status).toBe(200);
+  });
+
+  it('should return correct result', async () => {
+    const res = await request(app).get('/');
+    expect(res.text).toBe('Welcome to the payment system');
+  });
+
+  it('should have the correct content type', async () => {
+    const res = await request(app).get('/');
+    expect(res.header['content-type']).toMatch(/text\/html/);
+  });
+});
